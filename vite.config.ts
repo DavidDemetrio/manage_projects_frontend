d575f1be414cf94e880be16a8003df1669780698
DavidDemetrio <davidlopez.paz26@outlook.es>
2024-11-26 23:15:54 -0600

    Add @ to import default from ./src/*

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Especifica el CARACTER A USER para importar por default desde ./src/* (se sutituye por "@")
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
